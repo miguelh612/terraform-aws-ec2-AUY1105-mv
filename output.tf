fa7e886f05a3329f782c75393eb51f46dcfcb7c3
miguelh612 <mi.villagra@duocuc.cl>
2026-05-28 16:48:22 -0400

    Corrección en output duplicado

--- output.tf
+++ output.tf
@@ -7,13 +7,3 @@ output "ec2_instance_id" {
     description = "ID de la instancia EC2 creada"
   value = aws_instance.web.id
 }
-
-output "alb_dns_name" {
-  description = "DNS publico del Application Load Balancer"
-  value       = aws_lb.main.dns_name
-}
-
-output "alb_arn" {
-  description = "ARN del Application Load Balancer"
-  value       = aws_lb.main.arn
-}
